7472514c755aaba52b5913a2607b39a6f402acb0
John Bolos <johnbolos23@gmail.com>
2026-07-31 12:30:00 +0800

    update using latest source

--- templates/components/_event-card.php
+++ templates/components/_event-card.php
@@ -2,7 +2,7 @@
      <?php if (get_field('external_link')) { ?>
         <a href="<?php echo get_field('external_link'); ?>" class="event-link" target="<?php echo get_field('external_link_target'); ?>">
     <?php } else { ?>
-        <a href="<?php the_permalink(); ?>" class="event-link" target="_self">
+        <a href="<?= get_post_type() === 'event' ? '' : get_the_permalink(); ?>" class="event-link" target="_self">
     <?php }?>
         <span class="events-card-inner">
             <span class="image-container">
@@ -18,9 +18,12 @@
                 <span class="excerpt text-small">
                     <?php echo get_field('event_short_description_for_listing'); ?>
                 </span>
+                <?php if( empty(get_field('external_link')) && get_post_type() === 'event' ) : ?>
+                <?php else : ?>
                 <span class="link-container">
                     <span class="text-link red-text learn-more red-arrow-link arrow-link bold-link uppercase">Learn More</span>
                 </span>
+                <?php endif; ?>
             </span>
         </span>        
     </a>
