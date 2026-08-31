41024cc0ebdd07e3e2f552367bc087c89e7f367c
John Bolos <johnbolos23@gmail.com>
2026-08-31 12:13:10 +0800

    Fix DeprecatedParameters.Get_termsParam2Found in template-sectors-market-narrative.php
    - Merge deprecated positional $term_m arg into get_terms() args array

--- templates/template-sectors-market-narrative.php
+++ templates/template-sectors-market-narrative.php
@@ -518,7 +518,7 @@ $q_slug = $q->slug ?? '';
 		 <div class="container">
 			 <div id="loop" class="gridWrapper">
 				 <?php $term_m = 'sector-analysis';
-				  $terms = get_terms( $term_m, array(
+				  $terms = get_terms( array( 'taxonomy' => $term_m,
 					  'hide_empty' => false,
 				  ) );
 
