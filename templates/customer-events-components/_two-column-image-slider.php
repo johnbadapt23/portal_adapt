fe1d0bdbc283645f920a477d46a4137c295c6cab
John Bolos <johnbolos23@gmail.com>
2026-08-31 08:55:41 +0800

    Fix escaping in _two-column-image-slider.php
    esc_html() for title/text (text-column heading/text), list_item, button_text
    (via title/text slider-caption fields, 2x), form_button; esc_attr() for
    link_target, scroll_to_id.
    
    Incidental: trailing whitespace trimmed on two lines touched by the edit, no
    functional change.

--- templates/customer-events-components/_two-column-image-slider.php
+++ templates/customer-events-components/_two-column-image-slider.php
@@ -2,12 +2,12 @@
     <div class="container">
         <div class="column-container">
             <div class="column one-half text-column">
-                <h1 class="white-text"><?php echo get_sub_field( 'title' ); ?></h1>
-                <p class="p-large white-text"><?php echo get_sub_field( 'text' ); ?></p>
+                <h1 class="white-text"><?php echo esc_html( get_sub_field( 'title' ) ); ?></h1>
+                <p class="p-large white-text"><?php echo esc_html( get_sub_field( 'text' ) ); ?></p>
                 <?php if ( have_rows( 'list' ) ) : ?>
                     <span class="list-container">
                         <?php while ( have_rows( 'list' ) ) : the_row(); ?>
-                            <span class="list-item p-large red-tick-before"><?php echo get_sub_field( 'list_item' ); ?></span>
+                            <span class="list-item p-large red-tick-before"><?php echo esc_html( get_sub_field( 'list_item' ) ); ?></span>
                         <?php endwhile; ?>
                     </span>
                 <?php else : ?>
@@ -18,11 +18,11 @@
                     <span class="button-container">                                                                                                                   
                         <?php while ( have_rows( 'buttons' ) ) : the_row(); ?>
                             <?php if( get_sub_field( 'button_type' ) == 'link'){ ?> 
-                                <a class="stdBtn std-button <?php if($buttonCounter == 1){ ?>red-button<?php } else { ?>red-outline-button<?php } ?>" href="<?php echo esc_url( get_sub_field( 'button_link' ) ); ?>" target="<?php echo get_sub_field( 'link_target' ); ?>"><?php echo esc_html( get_sub_field( 'button_text' ) ); ?></a>
-                            <?php } else if( get_sub_field( 'button_type' ) =='scroll-to') { ?> 
-                                <a class="scroll-to-button std-button <?php if($buttonCounter == 1){ ?>red-button<?php } else { ?>red-outline-button<?php } ?>" href="#<?php echo get_sub_field( 'scroll_to_id' ); ?>"><?php echo esc_html( get_sub_field( 'button_text' ) ); ?></a>
-                            <?php } else { ?> 
-                                <span class="form-popup-button-container <?php if($buttonCounter == 1){ ?>red-button<?php } else { ?>red-outline-button<?php } ?>"><?php echo get_sub_field( 'form_button' ); ?></span>                                                               
+                                <a class="stdBtn std-button <?php if($buttonCounter == 1){ ?>red-button<?php } else { ?>red-outline-button<?php } ?>" href="<?php echo esc_url( get_sub_field( 'button_link' ) ); ?>" target="<?php echo esc_attr( get_sub_field( 'link_target' ) ); ?>"><?php echo esc_html( get_sub_field( 'button_text' ) ); ?></a>
+                            <?php } else if( get_sub_field( 'button_type' ) =='scroll-to') { ?>
+                                <a class="scroll-to-button std-button <?php if($buttonCounter == 1){ ?>red-button<?php } else { ?>red-outline-button<?php } ?>" href="#<?php echo esc_attr( get_sub_field( 'scroll_to_id' ) ); ?>"><?php echo esc_html( get_sub_field( 'button_text' ) ); ?></a>
+                            <?php } else { ?>
+                                <span class="form-popup-button-container <?php if($buttonCounter == 1){ ?>red-button<?php } else { ?>red-outline-button<?php } ?>"><?php echo esc_html( get_sub_field( 'form_button' ) ); ?></span>
                             <?php } ?>                                                                                                                                                                                                                                                                                                                                
                             <?php $buttonCounter++; ?>
                         <?php endwhile; ?>
@@ -46,8 +46,8 @@
                                     </span>
                                 </span>
                                 <span class="bottom-text-container">
-                                    <span class="label-XL white-text"><?php echo get_sub_field( 'title' ); ?></span>
-                                    <span class="label-Xsmall"><?php echo get_sub_field( 'text' ); ?></span>                                    
+                                    <span class="label-XL white-text"><?php echo esc_html( get_sub_field( 'title' ) ); ?></span>
+                                    <span class="label-Xsmall"><?php echo esc_html( get_sub_field( 'text' ) ); ?></span>
                                 </span>  
                                 <span class="progress">
                                     <span class="progress-inner"></span>
