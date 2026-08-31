b31b6aa389c028ad3d4c07995754b70a55afb0a0
John Bolos <johnbolos23@gmail.com>
2026-08-31 12:18:40 +0800

    Fix MissingArgDomain in template-persona-mapping.php
    - Add 'portal' text domain to esc_html_e() call

--- templates/template-persona-mapping.php
+++ templates/template-persona-mapping.php
@@ -507,7 +507,7 @@ $post_types = array ();
                     <?php } ?>
 
                 <?php endwhile; else : ?>
-                	<h2 class="h3"><?php esc_html_e( 'Sorry, no results found.' ); ?></h2>
+                	<h2 class="h3"><?php esc_html_e( 'Sorry, no results found.', 'portal' ); ?></h2>
                 <?php endif; ?>
                 <?php wp_pagenavi( array( 'query' => $posts ) ); ?>
                 <?php wp_reset_query();?>
