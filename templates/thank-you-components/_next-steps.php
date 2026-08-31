6aaf6c4bc0e0675c8a7e1874507c346d0a253abf
John Bolos <johnbolos23@gmail.com>
2026-08-31 10:06:08 +0800

    Escape output in templates/thank-you-components/_next-steps.php
    - esc_html() for title (2x), text
    - esc_html() for $counter (numeric step counter)
    
    All 4 WordPress.Security.EscapeOutput.OutputNotEscaped findings resolved. No other sniff categories in scope for this pass.

--- templates/thank-you-components/_next-steps.php
+++ templates/thank-you-components/_next-steps.php
@@ -1,7 +1,7 @@
 <section class="partners-cards-module next-steps-block">
 	<div class="container">
 		<div class="top-content">
-			<h2 class="text-black"><?php echo get_sub_field( 'title' ); ?></h2>
+			<h2 class="text-black"><?php echo esc_html( get_sub_field( 'title' ) ); ?></h2>
 		</div>
 		<div class="card-container">
 			<?php if ( have_rows( 'card' ) ) : ?>
@@ -16,13 +16,13 @@
 							<div class="counter-title-container">
 								<span class="counter-circle-outer">
 									<span class="counter circle-counter">
-										<?php echo $counter ?>
+										<?php echo esc_html( $counter ) ?>
 									</span>
 								</span>
 							</div>
-							<h3 class="black-text"><?php echo get_sub_field( 'title' ); ?></h3>
+							<h3 class="black-text"><?php echo esc_html( get_sub_field( 'title' ) ); ?></h3>
 							<span class="content-container-bottom">
-								<span class="card-text"><?php echo get_sub_field( 'text' ); ?></span>
+								<span class="card-text"><?php echo esc_html( get_sub_field( 'text' ) ); ?></span>
 							</span>
 						</span>
 					</div>
