1c5a5e689ed652f34949c3239e1976c6fe85e3ab
John Bolos <johnbolos23@gmail.com>
2026-08-31 09:39:50 +0800

    Fix escaping in _home-introduction.php
    esc_url() for $background_pattern['url']; esc_html() for introduction_text,
    title, text; esc_attr() for link_target.

--- templates/components/_home-introduction.php
+++ templates/components/_home-introduction.php
@@ -1,13 +1,13 @@
 <section class="advantage-home-introduction">
     <div class="container">
         <?php $background_pattern = get_sub_field( 'background_pattern' ); ?>
-        <div class="introduction-container" <?php if ( $background_pattern ) { ?>style="background-image: url(<?php echo $background_pattern['url']; ?>);"<?php } ?>>
-            <h1 class="introduction-title"><?php echo get_sub_field( 'introduction_text' ); ?></h1>
+        <div class="introduction-container" <?php if ( $background_pattern ) { ?>style="background-image: url(<?php echo esc_url( $background_pattern['url'] ); ?>);"<?php } ?>>
+            <h1 class="introduction-title"><?php echo esc_html( get_sub_field( 'introduction_text' ) ); ?></h1>
             <?php if ( have_rows( 'link_blocks' ) ) : ?>
                 <div class="introduction-link-container">
                     <?php while ( have_rows( 'link_blocks' ) ) : the_row(); ?>
                         <div class="introduction-link column one-third">
-                            <a href="<?php echo esc_url( get_sub_field( 'link' ) ); ?>" target="<?php echo get_sub_field( 'link_target' ); ?>">
+                            <a href="<?php echo esc_url( get_sub_field( 'link' ) ); ?>" target="<?php echo esc_attr( get_sub_field( 'link_target' ) ); ?>">
                                 <span class="link-inner">
                                     <span class="icon-container">
                                         <span class="icon-background-container">
@@ -25,8 +25,8 @@
                                             </span>
                                         </span>
                                     </span>
-                                    <h2 class="link-title"><?php echo get_sub_field( 'title' ); ?></h2>
-                                    <span class="text"><?php echo get_sub_field( 'text' ); ?></span>
+                                    <h2 class="link-title"><?php echo esc_html( get_sub_field( 'title' ) ); ?></h2>
+                                    <span class="text"><?php echo esc_html( get_sub_field( 'text' ) ); ?></span>
                                     <span class="readMore"><?php echo esc_html( get_sub_field( 'link_text' ) ); ?></span>
                                 </span>
                             </a>
