2b909830b0beb2b75beccfd7260b8743c7e73816
John Bolos <johnbolos23@gmail.com>
2026-08-31 10:05:32 +0800

    Escape output in templates/thank-you-components/_two-column.php
    - esc_html() for title, text, list_item
    - esc_attr() for link_target
    
    All 4 WordPress.Security.EscapeOutput.OutputNotEscaped findings resolved. No other sniff categories in scope for this pass.

--- templates/thank-you-components/_two-column.php
+++ templates/thank-you-components/_two-column.php
@@ -1,11 +1,11 @@
 <section class="two-column-thank-you-module background-black">
 	<div class="container">
 		<span class="title-container">
-			<h2 class="white-text"><?php echo get_sub_field( 'title' ); ?></h2>
+			<h2 class="white-text"><?php echo esc_html( get_sub_field( 'title' ) ); ?></h2>
 		</span>
 		<div class="column-container">
 			<div class="column text-list-column">
-				<span class="white-text text"><?php echo get_sub_field( 'text' ); ?></span>
+				<span class="white-text text"><?php echo esc_html( get_sub_field( 'text' ) ); ?></span>
 				<div class="mobile-image">
 					<?php $image = get_sub_field( 'image' ); ?>
 					<?php if ( $image ) { ?>
@@ -16,7 +16,7 @@
 					<span class="list-container">
 						<?php while ( have_rows( 'list_items' ) ) : the_row(); ?>
 							<span class="list-item white-text labelXLarge">
-								<?php echo get_sub_field( 'list_item' ); ?>
+								<?php echo esc_html( get_sub_field( 'list_item' ) ); ?>
 							</span>
 						<?php endwhile; ?>
 					</span>
@@ -26,7 +26,7 @@
 				<?php if ( have_rows( 'button' ) ) : ?>
 					<span class="button-container">
 						<?php while ( have_rows( 'button' ) ) : the_row(); ?>
-							<a class="std-button button-with-arrow red-button" href="<?php echo esc_url( get_sub_field( 'link' ) ); ?>" target="<?php echo get_sub_field( 'link_target' ); ?>"><?php echo esc_html( get_sub_field( 'link_text' ) ); ?></a>
+							<a class="std-button button-with-arrow red-button" href="<?php echo esc_url( get_sub_field( 'link' ) ); ?>" target="<?php echo esc_attr( get_sub_field( 'link_target' ) ); ?>"><?php echo esc_html( get_sub_field( 'link_text' ) ); ?></a>
 						<?php endwhile; ?>
 					</span>
 				<?php else : ?>
