1c2796bc3bdb791d7bb88b9958e6cc8f8430f031
John Bolos <johnbolos23@gmail.com>
2026-08-31 12:13:12 +0800

    Fix DeprecatedParameters.Get_termsParam2Found in template-technology-trends-markets.php
    - Merge deprecated positional $term_m arg into get_terms() args array

--- templates/template-technology-trends-markets.php
+++ templates/template-technology-trends-markets.php
@@ -524,7 +524,7 @@ $q_slug = $q->slug ?? '';
 		 <div class="container">
 			 <div id="loop" class="gridWrapper">
 				 <?php $term_m = 'topic';
-				  $terms = get_terms( $term_m, array(
+				  $terms = get_terms( array( 'taxonomy' => $term_m,
 					  'hide_empty' => false,
 				  ) );
 
