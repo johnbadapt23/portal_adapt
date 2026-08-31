f6e5d03b55e2a8534e464f5dacab38a630fdb549
John Bolos <johnbolos23@gmail.com>
2026-08-31 12:18:26 +0800

    Fix MissingArgDomain in template-dashboard.php
    - Add 'portal' text domain to esc_html_e() call

--- templates/template-dashboard.php
+++ templates/template-dashboard.php
@@ -59,7 +59,7 @@ if ( 0 == $current_user->ID ) {
                     <?php } ?>
 
                 <?php endwhile; else : ?>
-                	<h2 class="h3"><?php esc_html_e( 'Sorry, no results found.' ); ?></h2>
+                	<h2 class="h3"><?php esc_html_e( 'Sorry, no results found.', 'portal' ); ?></h2>
                 <?php endif; ?>
 
                 <?php wp_reset_postdata(); wp_reset_query();?>
