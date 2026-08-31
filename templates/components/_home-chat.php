0e30470a61eaff20b4f94fcae73e51c9287d6362
John Bolos <johnbolos23@gmail.com>
2026-08-31 10:14:17 +0800

    Escape output in templates/components/_home-chat.php
    - esc_html() for title, text, chat_button_text
    
    All 3 WordPress.Security.EscapeOutput.OutputNotEscaped findings resolved. No other sniff categories in scope for this pass.

--- templates/components/_home-chat.php
+++ templates/components/_home-chat.php
@@ -7,10 +7,10 @@
                     <?php echo wp_get_attachment_image( $image['ID'], 'full', false, array( 'alt' => $image['alt'] ) ); ?>
                 <?php } ?>
             </span>
-            <h2 class="chat-title"><?php echo get_sub_field( 'title' ); ?></h2>
-            <span class="text"><?php echo get_sub_field( 'text' ); ?></span>
+            <h2 class="chat-title"><?php echo esc_html( get_sub_field( 'title' ) ); ?></h2>
+            <span class="text"><?php echo esc_html( get_sub_field( 'text' ) ); ?></span>
             <span class="button-container">
-                <a class="button chat-button red-button" href="#"><?php echo get_sub_field( 'chat_button_text' ); ?></a>
+                <a class="button chat-button red-button" href="#"><?php echo esc_html( get_sub_field( 'chat_button_text' ) ); ?></a>
             </span>
         </div>
     </div>
