8150bd1002644e8d904abbb02ef16cf292565d78
John Bolos <johnbolos23@gmail.com>
2026-08-31 09:07:22 +0800

    Fix escaping in template-welcome.php
    phpcs:ignore for script_text_area (admin-authored raw JavaScript echoed
    directly inside a <script> tag; esc_html()/wp_kses_post() would corrupt the
    script); esc_attr() for vimeo_code (2x, URL path fragment); esc_html() for
    $first_name (current user's own first name), welcome_message,
    how_to_get_started_link_text; esc_url() for get_template_directory_uri()
    (close-icon src).

--- templates/template-welcome.php
+++ templates/template-welcome.php
@@ -7,7 +7,7 @@ get_header();
 ?>
 
 <script>
-    <?php echo get_field('script_text_area'); ?>
+    <?php echo get_field('script_text_area'); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- admin-authored raw JavaScript output inside a <script> tag; esc_html()/wp_kses_post() would corrupt the script instead of escaping HTML. ?>
 </script>
 
 <?php
@@ -28,7 +28,7 @@ get_header();
         <div class="container">
             <div class="right">
                 <?php if( get_field('vimeo_code')){ ?>
-                    <a href="https://vimeo.com/<?php echo get_field('vimeo_code'); ?>" class="image popup-vimeo">
+                    <a href="https://vimeo.com/<?php echo esc_attr( get_field('vimeo_code') ); ?>" class="image popup-vimeo">
                 <?php } else { ?>
                     <a href="#" class="image <?php if (get_field('video_url')){ ?>postPlayBtn<?php } ?>">
                 <?php } ?>
@@ -51,23 +51,23 @@ get_header();
             </div>
             <div class="left">
                 <span class="welcome-message">
-                    Welcome, <br/><?php echo $first_name; ?>
+                    Welcome, <br/><?php echo esc_html( $first_name ); ?>
                 </span>
                 <span class="message">
-                    <?php echo get_field('welcome_message'); ?>
+                    <?php echo esc_html( get_field('welcome_message') ); ?>
                 </span>
                 <span class="button-block">
                     <?php if(get_field('button_link')){ ?>
                     <a <?php if(get_field('button_link')){ ?>href="<?php echo esc_url( get_field('button_link') ); ?>"<?php } ?> class="stdBtn red" target="_self"><?php echo esc_html( get_field('button_text') ); ?></a>
                     <?php } ?>
                     <?php if( get_field('how_to_get_started_link_text')){ ?>
-                        <a href="https://vimeo.com/<?php echo get_field('vimeo_code'); ?>" class="popup-vimeo stdBtn red" target="_self"><?php echo get_field('how_to_get_started_link_text'); ?></a>
+                        <a href="https://vimeo.com/<?php echo esc_attr( get_field('vimeo_code') ); ?>" class="popup-vimeo stdBtn red" target="_self"><?php echo esc_html( get_field('how_to_get_started_link_text') ); ?></a>
                     <?php } ?>
                 </span>
             </div>
             <?php if (get_field('video_url')){ ?>
                 <div class="videoPlayerContainer print-no">
-                    <span class="closeVideo"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/close-grey.svg" width="25" height="25" loading="lazy" decoding="async" alt="Close" /></span>
+                    <span class="closeVideo"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/close-grey.svg" width="25" height="25" loading="lazy" decoding="async" alt="Close" /></span>
                     <div class="videoWrapper">
                         <video width="100%" id="popupVideo" controls controlsList="nodownload">
                             <source type="video/mp4" src="<?php echo esc_url( get_field('video_url') ); ?>" />
