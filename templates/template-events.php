1fe0e6a3e024c6e8224e2d170c69e2305612c03e
John Bolos <johnbolos23@gmail.com>
2026-08-31 12:18:30 +0800

    Fix MissingArgDomain in template-events.php
    - Add 'portal' text domain to esc_html_e() call

--- templates/template-events.php
+++ templates/template-events.php
@@ -228,7 +228,7 @@ get_header();
                     </a>
                     <?php $counter++; ?>
                 <?php endwhile; else : ?>
-                	<h2 class="h3"><?php esc_html_e( 'Sorry, no results found.' ); ?></h2>
+                	<h2 class="h3"><?php esc_html_e( 'Sorry, no results found.', 'portal' ); ?></h2>
                 <?php endif; ?>
 
                 <?php wp_reset_postdata(); wp_reset_query();?>
