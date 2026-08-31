5db0de262e00553791edede4c8f23e8bdc4a4826
John Bolos <johnbolos23@gmail.com>
2026-08-31 10:19:00 +0800

    Escape output in templates/benchmark-components/_single-quote.php
    - esc_attr() for padding_top, padding_bottom (CSS class fragments), $target (link_target)
    
    All 3 WordPress.Security.EscapeOutput.OutputNotEscaped findings resolved. No other sniff categories in scope for this pass.

--- templates/benchmark-components/_single-quote.php
+++ templates/benchmark-components/_single-quote.php
@@ -1,4 +1,4 @@
-<section class="quote-slider customer-events-slider single-quote benchmarking-quote background-black <?php echo get_sub_field( 'padding_top' ); ?> <?php echo get_sub_field( 'padding_bottom' ); ?>">
+<section class="quote-slider customer-events-slider single-quote benchmarking-quote background-black <?php echo esc_attr( get_sub_field( 'padding_top' ) ); ?> <?php echo esc_attr( get_sub_field( 'padding_bottom' ) ); ?>">
 	<div class="container">
 		<div class="quote-slider-outer background-tertiary-black">
 			<div class="quote-module">
@@ -26,7 +26,7 @@
                             <span class="link-container">
                                 <?php $target = get_sub_field( 'link_target' ); ?>
                                 <?php if($target){ ?> 
-                                    <a class="red-text text-link large-link-text red-underline-link external-link" href="<?php echo esc_url( get_sub_field('full_story_link') ); ?>" target="<?php echo $target; ?>">Read full story</a>
+                                    <a class="red-text text-link large-link-text red-underline-link external-link" href="<?php echo esc_url( get_sub_field('full_story_link') ); ?>" target="<?php echo esc_attr( $target ); ?>">Read full story</a>
                                 <?php } else { ?> 
                                     <a class="red-text text-link large-link-text red-underline-link external-link" href="<?php echo esc_url( get_sub_field('full_story_link') ); ?>" target="_self">Read full story</a>
                                 <?php } ?>                                
