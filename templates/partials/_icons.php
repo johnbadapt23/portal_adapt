8e5329309e354aae44425377b1ae495710f4e653
John Bolos <johnbolos23@gmail.com>
2026-08-31 09:44:15 +0800

    Fix escaping in _icons.php
    esc_url() for get_template_directory_uri() (5x, favicon/manifest hrefs inside
    a commented-out HTML block that is still PHP-executed).

--- templates/partials/_icons.php
+++ templates/partials/_icons.php
@@ -1,7 +1,7 @@
-<!-- <link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/assets/icons/apple-touch-icon.png">
-<link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/assets/icons/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/assets/icons/favicon.ico">
-<link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/assets/icons/site.webmanifest">
-<link rel="mask-icon" href="<?php echo get_template_directory_uri(); ?>/assets/icons/safari-pinned-tab.svg" color="#5bbad5">
+<!-- <link rel="apple-touch-icon" sizes="114x114" href="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/icons/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/icons/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/icons/favicon.ico">
+<link rel="manifest" href="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/icons/site.webmanifest">
+<link rel="mask-icon" href="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/icons/safari-pinned-tab.svg" color="#5bbad5">
 <meta name="msapplication-TileColor" content="#da532c">
 <meta name="theme-color" content="#ffffff"> -->
